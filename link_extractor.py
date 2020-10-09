3897dd8f2720b13d5448737434681b9a39e33304
Pedro Soares <pedro@MBP-de-Pedro.fritz.box>
2020-10-09 13:53:25 +0200

    Maybe final version

--- link_extractor.py
+++ link_extractor.py
@@ -1,23 +1,26 @@
 """
 This module is capable to extract all the links form a given URL
 """
+from urllib import request
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 import requests
+
 
 def links_extractor(URL, internal_urls=[]):
     """
     Param : URL from the website to extract all the links
     """
-    response = requests.get(URL) #stores the html source code and returns the response
-    if response:
-        html_code = BeautifulSoup(response.content, "html.parser")
+    print('SEARCHING: ', URL)
+    if requests.get(URL).status_code !=404:
+        html_code = BeautifulSoup(requests.get(URL).content, "html.parser")
         for a in html_code.findAll('a'):
             href = a.get('href')
             if not urlparse(href).scheme and urlparse(href).path:
                 parsed_url = urljoin(URL, href)
-                if requests.get(parsed_url) and not parsed_url in internal_urls:
+                if requests.get(parsed_url).status_code !=404 and not parsed_url in internal_urls:
                     internal_urls.append(parsed_url)
+                    print(len(internal_urls))
     return internal_urls
 
 def all_links_extractor(URL, max_iterations=10):
@@ -26,20 +29,25 @@ def all_links_extractor(URL, max_iterations=10):
     return a sorted list of all the internal urls
     """
     curr_iteration = 0
-    all_internal_urls = links_extractor(URL)
+    all_internal_urls = [URL]
     
     for link in all_internal_urls:
         curr_iteration+=1
+        print()
+        print('ITERATION NR:', curr_iteration)
+        print()
         if curr_iteration<=max_iterations:
             all_internal_urls = links_extractor(link, all_internal_urls)
+        else: break
     all_internal_urls.sort()
     return all_internal_urls
+
 
 import cProfile, pstats, io
 from pstats import SortKey
 pr = cProfile.Profile()
 pr.enable()
-all_links_extractor('https://wwwfr.uni.lu')
+print(all_links_extractor('https://infallible-varahamihira-e94f86.netlify.app'))
 pr.disable()
 s = io.StringIO()
 sortby = SortKey.CUMULATIVE
