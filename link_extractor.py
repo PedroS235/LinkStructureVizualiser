6d8568afc8b607329d4ad7721d0d50ecdb92c8ec
Pedro Soares <pedro@MBP-de-Pedro.fritz.box>
2020-10-18 21:06:55 +0200

    Added digraph

--- link_extractor.py
+++ link_extractor.py
@@ -1,17 +1,20 @@
 """
 This module is capable to extract all the links form a given URL
 """
-from urllib import request
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 import requests
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
+G = nx.DiGraph()
 internal_urls = []
 external = []
+
 def isvalid(url):
+    """
+    returns TRUE if the 'url' is valid and FALSE if the 'url is not valid
+    """
     parsed = urlparse(url)
     return bool(parsed.scheme) and bool(parsed.netloc)
 
@@ -29,37 +32,32 @@ def get_website_links(url):
             href = urljoin(url, href)
             parsed_href = urlparse(href)
             href = parsed_href.scheme + '://' + parsed_href.netloc + parsed_href.path
-            if isvalid:
-                if not href in internal_urls and domain_name in href:
-                    if requests.get(href):
-                        internal_urls.append(href)
-                        print(href)
+            if isvalid(href):
+                #if not href in internal_urls and domain_name in href:
+                if domain_name in href:
+                    #if requests.get(href):
+                    internal_urls.append(href)
+                    G.add_edge(url, href)
                 else: external.append(href)
 
-def crawl(URL, max_iterations=3):
+def crawl(url, max_iterations=3):
     """
     Param: URL from a website | max_iteration defines how deep we extract links
     return a sorted list of all the internal urls
     """
     curr_iteration = 0
-    internal_urls.append(URL)
+    internal_urls.append(url)
     for link in internal_urls:
         curr_iteration+=1
         print(curr_iteration)
         if curr_iteration<=max_iterations:
             get_website_links(link)
         else: break
-
-import cProfile, pstats, io
-from pstats import SortKey
-pr = cProfile.Profile()
-pr.enable()
+        
 crawl('https://wwwfr.uni.lu')
-pr.disable()
-s = io.StringIO()
-sortby = SortKey.CUMULATIVE
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-print(s.getvalue())
-print('Internal URLS: ', len(internal_urls))
-print('External URLS: ', len(internal_urls))
+val_map = {'https://wwwfr.uni.lu': 1.0}
+
+values = [val_map.get(node, 0.25) for node in G.nodes()]
+
+nx.draw(G, node_color = values, with_labels=True, font_size=8)
+plt.show()
