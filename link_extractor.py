b990f11d88c19ceded9db7c17af4ff4d3d4fbab7
Pedro Soares <pedro@MBP-de-Pedro.fritz.box>
2020-10-25 16:17:29 +0100

    starting the grapg part

--- link_extractor.py
+++ link_extractor.py
@@ -4,12 +4,11 @@ This module is capable to extract all the links form a given URL
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 import requests
-import networkx as nx
-import matplotlib.pyplot as plt
 
-G = nx.DiGraph()
-internal_urls = []
-external = []
+
+
+nodes = []
+edges = []
 
 def isvalid(url):
     """
@@ -28,17 +27,18 @@ def get_website_links(url):
 
     for a_tag in soup.findAll('a'):
         href = a_tag.get('href')
-        if href !='':
+        if href !='' or href != None:
             href = urljoin(url, href)
             parsed_href = urlparse(href)
             href = parsed_href.scheme + '://' + parsed_href.netloc + parsed_href.path
             if isvalid(href):
                 #if not href in internal_urls and domain_name in href:
                 if domain_name in href:
-                    #if requests.get(href):
-                    internal_urls.append(href)
-                    G.add_edge(url, href)
-                else: external.append(href)
+                    #if requests.get(href): #I removed the checker for doubles
+                    edges.append((url, href))
+                    if not href in nodes:
+                        nodes.append(href)
+                        
 
 def crawl(url, max_iterations=3):
     """
@@ -46,18 +46,14 @@ def crawl(url, max_iterations=3):
     return a sorted list of all the internal urls
     """
     curr_iteration = 0
-    internal_urls.append(url)
-    for link in internal_urls:
+    nodes.append(url)
+    for link in nodes:
         curr_iteration+=1
         print(curr_iteration)
         if curr_iteration<=max_iterations:
             get_website_links(link)
         else: break
-        
-crawl('https://wwwfr.uni.lu')
-val_map = {'https://wwwfr.uni.lu': 1.0}
+    return edges
 
-values = [val_map.get(node, 0.25) for node in G.nodes()]
-
-nx.draw(G, node_color = values, with_labels=True, font_size=8)
-plt.show()
+def get_nodes():
+    return nodes
