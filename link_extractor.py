5e3accd2f58c7c284b7001eaa40f559b9e8d2be1
Pedro Soares <pmbs.12@hotmail.com>
2020-11-01 10:08:01 +0100

    modified

--- link_extractor.py
+++ link_extractor.py
@@ -5,10 +5,13 @@ from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 import requests
 
+#variable to store the different nodes categorized by groups
+nodes = [] # [{'id":'url', 'group':1}]
+#variable to store the different edges categorized by values(wheights)
+edges = [] # [{'source":'url1', 'target':'url2', 'value':1}]
+links = []
 
-
-nodes = []
-edges = []
+value=1
 
 def isvalid(url):
     """
@@ -17,6 +20,7 @@ def isvalid(url):
     parsed = urlparse(url)
     return bool(parsed.scheme) and bool(parsed.netloc)
 
+
 def get_website_links(url):
     """
     Returns all links that are found on 'url'
@@ -24,30 +28,39 @@ def get_website_links(url):
     #domain name of the 'url' without the protocol
     domain_name = urlparse(url).netloc
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
-
+    group_=1
+    it=0
     for a_tag in soup.findAll('a'):
+        it+=1
+        group_+=1
         href = a_tag.get('href')
         if href !='' or href != None:
             href = urljoin(url, href)
             parsed_href = urlparse(href)
             href = parsed_href.scheme + '://' + parsed_href.netloc + parsed_href.path
-            if isvalid(href):
-                #if not href in internal_urls and domain_name in href:
-                if domain_name in href:
+            if isvalid(href) and domain_name in href:
+                #if requests.get(href):
+                if href in links:
+                    edges.append({'source':url, 'target':href, 'value':2})
+                else:
+                    edges.append({'source':url, 'target':href, 'value':1})
+                if not href in links:
                     #if requests.get(href): #I removed the checker for doubles
-                    edges.append((url, href))
-                    if not href in nodes:
-                        nodes.append(href)
-                        
+                    if(group_>=2):
+                        group_ = 2
+                    
+                    nodes.append({'id':href, 'group':group_})
+                    links.append(href)
 
-def crawl(url, max_iterations=3):
+def crawl(url, max_iterations=6):
     """
     Param: URL from a website | max_iteration defines how deep we extract links
     return a sorted list of all the internal urls
     """
     curr_iteration = 0
-    nodes.append(url)
-    for link in nodes:
+    nodes.append({'id':url, 'group':1})
+    links.append(url)
+    for link in links:
         curr_iteration+=1
         print(curr_iteration)
         if curr_iteration<=max_iterations:
@@ -55,5 +68,81 @@ def crawl(url, max_iterations=3):
         else: break
     return edges
 
-def get_nodes():
-    return nodes
+crawl('https://networkx.org/documentation/stable/reference/generators.html')
+
+import networkx as nx
+import matplotlib.pyplot as plt
+G = nx.DiGraph()
+
+#add the nodes to the graph G with attributes
+for node in nodes:
+    G.add_node(node['id'], group=node['group'])
+
+#add edges to the graph G with wheights
+for edge in edges:
+    G.add_edge(edge['source'], edge['target'], value=edge['value'])
+"""
+node_color = []
+# for each node in the graph
+for node in G.nodes(data=True):
+
+    if 1 == node[1]['group']:
+        node_color.append('blue')
+    
+    if 2 == node[1]['group']:
+        node_color.append('orange')
+    
+    if 3 == node[1]['group']:
+        node_color.append('yellow')
+    if 4 == node[1]['group']:
+        node_color.append('red')
+    if 5 == node[1]['group']:
+        node_color.append('green')
+    if 6 == node[1]['group']:
+        node_color.append('purple')
+    if 7 == node[1]['group']:
+        node_color.append('green')
+    if 8 == node[1]['group']:
+        node_color.append('purple')
+    if 9 == node[1]['group']:
+        node_color.append('green')
+    if 10 == node[1]['group']:
+        node_color.append('purple')
+
+node_size=[]
+for node in G.nodes(data=True):
+    if 1 == node[1]['group'] :
+        node_size.append(500*1)
+    
+    if 2 == node[1]['group']:
+        node_size.append(200*1)
+    
+    if 3 == node[1]['group']:
+        node_size.append(200*0.8)
+    if 4 == node[1]['group']:
+        node_size.append(200*0.7)
+    if 5 == node[1]['group']:
+        node_size.append(200*0.6)
+    if 6 == node[1]['group']:
+        node_size.append(200*0.5)
+    if 7 == node[1]['group']:
+        node_size.append(200*0.5)
+    if 8 == node[1]['group']:
+        node_size.append(200*0.5)
+    if 9 == node[1]['group']:
+        node_size.append(200*0.5)
+    if 10 == node[1]['group']:
+        node_size.append(200*0.5)
+"""
+"""
+edge_color=[]
+
+for edge in G.edges(data=True):
+    if edge[2]['value']==1:
+        edge_color.append('violet')
+    if edge[2]['value']==2:
+        edge_color.append('gray') 
+"""
+
+nx.draw(G, with_labels=False, width=2)
+plt.show()
